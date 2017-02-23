7db63f24c3c5985c852039021bd82ee1c684633b
ScoutOK <ohkeefe@gmail.com>
2017-02-23 17:08:50 -0500

    added react to the html

---
+++ app/react/Main.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <h2>This is coming from React!</h2>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import webpack from 'webpack';
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+              test: /jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel',
+              query: {
+                presets: ['react', 'es2015']
+              }
+            }
+        ]
+    }
+};
