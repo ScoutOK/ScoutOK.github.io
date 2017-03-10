a18eb7c624ead50539a7d9cb3783eb9998be82f5
ScoutOK <ohkeefe@gmail.com>
2017-03-10 16:01:55 -0500

    closes #13

--- app/react/Navbar.js
+++ app/react/Navbar.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Scroll, {Link} from 'react-scroll';
+
 
 export default (props) => {
   return (
     <nav>
       <ul>
-        <li className='active'><a href='#top'>Home</a></li>
-        <li><a href='#projects'>Projects</a></li>
-        <li><a href='#presentations'>Presentations</a></li>
-        <li><a href='#design'>Design Work</a></li>
-        <li><a href='#contact'>Contact</a></li>
+        <li><Link activeClass='active' to='top' spy={true} smooth={true}>Home</Link></li>
+        <li><Link activeClass='active' to='projects' spy={true} smooth={true}>Projects</Link></li>
+        <li><Link activeClass='active' to='presentations' spy={true} smooth={true}>Presentations</Link></li>
+        <li><Link activeClass='active' to='design' spy={true} smooth={true}>Design Work</Link></li>
+        <li><Link activeClass='active' to='contact' spy={true} smooth={true}>Contact</Link></li>
       </ul>
     </nav>
   )
