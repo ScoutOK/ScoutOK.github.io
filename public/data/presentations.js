a66b872133a846cac4d99202ec45a28b0648659d
ScoutOK <ohkeefe@gmail.com>
2017-03-23 11:10:35 -0400

    added openCV to data

--- public/data/presentations.js
+++ public/data/presentations.js
@@ -1,5 +1,11 @@
 module.exports = [
   {
+    id: 0,
+    name:'Introduction to OpenCV',
+    videoUrl:'https://www.youtube.com/watch/oI1GgyRU5a4',
+    description:'In this video I talk about computer vision with a specific focus on the OpenCV library. Scout explains the basics of OpenCV such as the basic data structure and how the library handles object detection. Scout finishes up the presentation by displaying some brief live examples of some basic functions of OpenCV, including face detection, through a web cam.',
+  },
+  {
     id: 1,
     name:'Motions',
     videoUrl:'https://www.youtube.com/embed/CMZ9WmhWtSc',
