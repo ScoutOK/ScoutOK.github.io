5222b4dde892986dbbaa9c484d36ddd850ff59a0
ScoutOK <ohkeefe@gmail.com>
2017-04-11 15:26:50 -0400

    added slam and mbta artworks

--- public/data/designs.js
+++ public/data/designs.js
@@ -9,14 +9,22 @@ module.exports = [
   },
   {
     id: 2,
+    name:'ADA 24th Anniversary Celebration: Poetry Slam',
+    type: 'Print Media',
+    year: '2015',
+    image: 'public/img/SlamFlyer.png',
+    alt: 'poster featuring photos of poets and microphone',
+  },
+  {
+    id: 3,
     name: 'Urinetown Poster',
     type: 'Print Media',
-    year: '2014',
+    year: '2012',
     image: 'public/img/urinetown.png',
     alt: 'image in protest art fashion of young man holding up plunger'
   },
   {
-    id: 3,
+    id: 4,
     name: 'Rocky Horror Show Poster',
     type: 'Print Media',
     year: '2013',
@@ -24,11 +32,19 @@ module.exports = [
     alt: 'image of backlit muscular person in lingerie'
   },
   {
-    id: 4,
+    id: 5,
     name: 'Institute for Human Centered Design',
     type: 'Website Design',
     year: '2015',
     image: 'public/img/ihcd.png',
+    alt: 'homepage for the Institute for Human Centered Design'
+  },
+  {
+    id: 6,
+    name: 'MBTA Ad for CPL Conference',
+    type: 'Print Design',
+    year: '2011',
+    image: 'public/img/mbta.png',
     alt: 'homepage for the Institute for Human Centered Design'
   }
 ]
