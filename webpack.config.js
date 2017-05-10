da17fb0924b1299a3b9e7d198b1fd98c7b43cae3
ScoutOK <ohkeefe@gmail.com>
2017-05-10 12:27:15 -0400

    added production environment

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,13 @@ module.exports = {
               }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
